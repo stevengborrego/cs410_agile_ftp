6d404e4432858d280b67459f04ebb8343df1fd33
stevengborrego <sborrego@pdx.edu>
2022-07-27 12:41:42 -0700

    create skeleton unit test file

---
+++ test_ftp.py
@@ -0,0 +1,16 @@
+import unittest
+import ftp
+import mock
+from mock import patch
+
+class TestModule(unittest.TestCase):
+    def test_module(self):
+        pass
+
+    # test that ftp.dir() is called
+    def test_list_directories_and_files(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
